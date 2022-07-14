2bedd4e5a586efde39b93b60dd7d97723bf3ef9f
manan228 <manan228@yahoo.in>
2022-07-15 00:06:24 +0530

    Task 10 Completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
 
@@ -14,6 +15,19 @@ function App() {
     fetchMovieHandler();
   }, [])
 
+  async function addMovieHandler(movie) {
+
+    const response = await fetch('https://react-http-b7832-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json();
+    console.log(data);
+  }
+
   async function fetchMovieHandler() {
 
     setIsLoading(true)
@@ -21,23 +35,23 @@ function App() {
     
     try {
 
-      const response = await fetch('https://swapi.dev/api/films/')
+      const response = await fetch('https://react-http-b7832-default-rtdb.firebaseio.com/movies.json')
       if (!response.ok) {
         throw new Error('Somwthing went wrong!');
       }
       const data = await response.json();
 
-    const transformedMovies = data.results.map((movieData) => {
+      const loadedMovies = [];
 
-      return {
-
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date
+      for(const key in data) {
+        loadedMovies.push({
+          id: KeyboardEvent,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        })
       }
-    })
-    setMovies(transformedMovies);
+    setMovies(loadedMovies);
     setIsLoading(false)
     }
 
@@ -49,6 +63,9 @@ function App() {
   }
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler}/>
+      </section>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
