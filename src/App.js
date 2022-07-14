39de56cbcee06c28f7a24b101326567b533dd9db
manan228 <manan228@yahoo.in>
2022-07-14 22:50:26 +0530

    Task 9 Completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -7,7 +7,12 @@ function App() {
 
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('')
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+
+    fetchMovieHandler();
+  }, [])
 
   async function fetchMovieHandler() {
 
